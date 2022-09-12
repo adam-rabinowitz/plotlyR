144bc3c10d62945709338716857935e0c60fb875
Adam Rabinowitz <adamrabs@gmail.com>
2022-09-12 19:08:51 +0200

    added functiion to add horizontal and vertical lines to plot

--- R/formatting.R
+++ R/formatting.R
@@ -28,6 +28,60 @@ add_titles <- function(
   return(fig)
 }
 
+#' Add veritcal and horizontal lines to plot
+#'
+#' @param fig a plotly plot
+#' @param yintercepts y-axis intercept of horizontal lins
+#' @param xintercepts x-axis intercept of vertical lines
+#' @param color color of lines
+#' @param opacity opacity of lines
+#' @return The formatted plotly plot
+#' @export
+add_lines <- function(
+    fig, yintercepts=NULL, xintercepts=NULL, color='black', opacity=1
+) {
+  # Check arguments
+  if (length(c(yintercepts, xintercepts)) == 0) {
+    stop('one of xintercepts and xintercepts must be supplied')
+  }
+  if (opacity < 0 | opacity > 1) {
+    stop('opacity must be between 0 and 1')
+  }
+  # Create shapes for horizontal lines
+  h_lines = list()
+  for (i in seq_along(yintercepts)) {
+    h_lines[[i]] <- list(
+      type = "line",
+      x0 = 0,
+      x1 = 1,
+      xref = "paper",
+      y0 = yintercepts[i],
+      y1 = yintercepts[i],
+      line = list(color = color),
+      opacity=opacity
+    )
+  }
+  # Create shapes for vertical lines
+  v_lines = list()
+  for (i in seq_along(xintercepts)) {
+    v_lines[[i]] <- list(
+      type = "line",
+      x0 = xintercepts[i],
+      x1 = xintercepts[i],
+      y0 = 0,
+      y1 = 1,
+      yref = "paper",
+      line = list(color = color),
+      opacity=opacity
+    )
+  }
+  # Create shapes, apply and return
+  shapes <- c(h_lines, v_lines)
+  fig <- fig %>% plotly::layout(shapes=shapes)
+  return(fig)
+}
+
+
 #' Apply default formatting to plot
 #'
 #' @param fig a plotly plot
